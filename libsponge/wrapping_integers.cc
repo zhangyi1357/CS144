25c37d9aa0db9c01063983ba9363c291c5386410
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2023-02-08 01:00:59 +0800

    lab2: seqno wrap and unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,22 +1,22 @@
 #include "wrapping_integers.hh"
 
+#include <cstdint>
+#include <iostream>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32(static_cast<uint32_t>(n) + isn.raw_value()); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +29,17 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t up_mask = 0xFFFFFFFF00000000;
+    uint64_t up_checkpoint = checkpoint & up_mask;
+    uint64_t result = n.raw_value() - isn.raw_value() + up_checkpoint;
+    if (result > checkpoint) {
+        if (up_checkpoint != 0 && result - checkpoint > (1ul << 31)) {
+            result -= (1ul << 32);
+        }
+    } else {
+        if (up_checkpoint != up_mask && checkpoint - result > (1ul << 31)) {
+            result += (1ul << 32);
+        }
+    }
+    return result;
 }
